a2e880d63c6a446a7870ecacb3eb67707885c05e
yeongmin <dudals1069@gmail.com>
2022-12-09 12:41:14 +0900

    AOP

--- sistStudy/AopTest/src/main/java/com/example/demo/common/DaoCommon.java
+++ sistStudy/AopTest/src/main/java/com/example/demo/common/DaoCommon.java
@@ -1,6 +1,8 @@
 package com.example.demo.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -13,13 +15,44 @@ public class DaoCommon {
 	@Pointcut("execution(public * com.example.demo.dao..*(..))")
 	public void daoCommon() {}
 	
+	@Around("daoCommon()")
+	public Object around(ProceedingJoinPoint joinPoint) {
+		Object obj = null;
+		String methodName = joinPoint.getSignature().getName();
+		System.out.println(methodName+"가 동작하기 전입니다.");
+		try {
+			obj = joinPoint.proceed();
+		}catch (Throwable e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(methodName+"가 완료되었습니다.");
+		return obj;
+	}
+	
+	
+	/*
 	@Before("daoCommon()")
 	public void pro(JoinPoint joinpoint) {
 		String methodName1 = joinpoint.getSignature().toLongString();
 		String methodName2 = joinpoint.getSignature().toShortString();
 		System.out.println(methodName1);
 		System.out.println(methodName2);
+		
+		Object targetObj = joinpoint.getTarget();
+		System.out.println("타깃:"+targetObj);
+		Object []args = joinpoint.getArgs();
+		System.out.println("매개변수 목록");
+		if(args != null) {
+			for(Object obj : args) {
+				System.out.println(obj);
+			}
+		}
+		
 		System.out.println("DAO가 처리되기전에 동작하는 공통기능입니다.");
 		System.out.println("------------------------------------------");
 	}
+	*/
+	
+	
 }
