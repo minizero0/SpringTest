954a238a9ded0ea5ffdc24d1277c787b9586da9a
yeongmin <dudals1069@gmail.com>
2022-12-12 12:51:07 +0900

    security Spring

--- sistStudy/securitytest/src/main/java/com/example/demo/SpringConfig.java
+++ sistStudy/securitytest/src/main/java/com/example/demo/SpringConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -7,6 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SpringConfig {
 	
+	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
